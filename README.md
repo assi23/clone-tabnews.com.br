c414c141a4ac0e1c90b6dc8e7fdea2d65745e1ab
Guilherme Assi <guiassi@hotmail.com>
2023-10-04 23:09:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews.com.br
+
 Projeto de estudo e recriação do tabnews do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-function Home(){
-    return <h1>"Se você deseja fazer uma torta de maçã do zero, 
-        você deve, primeiro criar o universo." ~Carl Sagan</h1>
+function Home() {
+  return (
+    <h1>
+      "Se você deseja fazer uma torta de maçã do zero, você deve, primeiro criar
+      o universo." ~Carl Sagan
+    </h1>
+  );
 }
 
 export default Home;
